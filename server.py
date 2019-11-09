0f0e1a928773f8ae51e46d83051577234e4aed72
Muhammed Kilicarslan <mka281@icloud.com>
2019-11-09 13:36:38 -0500

    Set up HTML template and routes

--- server.py
+++ server.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def about(page_name):
+    return render_template(page_name)
