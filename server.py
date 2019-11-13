a489666cb7d67fd0806c418c268e0fe013b3722c
Muhammed Kilicarslan <mka281@icloud.com>
2019-11-13 10:16:41 -0500

    Save contact page inputs to a CSV file and redirect to thankyou page

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import csv
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
@@ -8,5 +9,32 @@ def homepage():
 
 
 @app.route('/<string:page_name>')
-def about(page_name):
+def html_page(page_name):
     return render_template(page_name)
+
+
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as csvfile:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        writer = csv.writer(csvfile)
+        writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong!'
